89c0771fc9206a960f23123303a09e381b80e3ac
scolp <scolpig@gmail.com>
2024-02-02 15:14:57 +0900

    final

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -17,17 +17,36 @@ df_reviews = pd.read_csv('./cleaned_one_review.csv')
 Tfidf_matrix = mmread('./models/Tfidf_movie_review.mtx').tocsr()
 with open('./models/tfidf.pickle', 'rb') as f:
     Tfidf = pickle.load(f)
-ref_idx = 10
-print(df_reviews.iloc[ref_idx, 0])
-consine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
-print(consine_sim[0])
-print(len(consine_sim))
-recommandation = getRecommendation(consine_sim)
-print(recommandation)
 
+# 영화 index 이용
+# ref_idx = 10
+# print(df_reviews.iloc[ref_idx, 0])
+# consine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
+# print(consine_sim[0])
+# print(len(consine_sim))
+# recommandation = getRecommendation(consine_sim)
+# print(recommandation)
 
+# keyword 이용
 
+embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
+keyword = '드래곤'
+sim_word = embedding_model.wv.most_similar(keyword, topn=10)
+words = [keyword]
+for word, _ in sim_word:
+    words.append(word)
+setence = []
+count = 10
+for word in words:
+    setence = setence + [word] * count
+    count -= 1
+setence = ' '.join(setence)
+print(setence)
+setence_vec = Tfidf.transform([setence])
+cosine_sim = linear_kernel(setence_vec, Tfidf_matrix)
+recommendation = getRecommendation(cosine_sim)
 
+print(recommendation)
 
 
 
