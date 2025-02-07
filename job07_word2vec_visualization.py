1a4e4001131576dcdabaf66cb0156414ef237b1f
buskingsue <abcdzip@naver.com>
2025-02-07 12:21:36 +0900

    pca설명 추가 1.차원 축소 2.노이즈제거 3.특징 추출

--- job07_word2vec_visualization.py
+++ job07_word2vec_visualization.py
@@ -11,10 +11,11 @@ mpl.rcParams['axes.unicode_minus'] = False
 rc('font', family=font_name)
 
 embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
-key_word = '매력'
+key_word = '재미있다'
 sim_word = embedding_model.wv.most_similar(key_word, topn=10)
 print(sim_word)
-
+print(list(embedding_model.wv.index_to_key)) #학습된 단어의 목록
+print(len(embedding_model.wv.index_to_key)) #학습된 단어의 총 개수
 vectors = []
 labels = []
 
@@ -29,6 +30,21 @@ print(df_vectors.head())
 
 tsne_model = TSNE(perplexity=9, n_components=2, init='pca', n_iter=2500)
 new_value = tsne_model.fit_transform(df_vectors)
+
+# TSNE를 사용하여 100차원 벡터를 2차원으로 축소
+# perplexity=9 → 데이터의 밀도를 고려하는 매개변수
+# n_components=2 → 2차원으로 변환
+# init='pca' → 초기값을 PCA 방식으로 설정
+
+# PCA의 주요 목적
+# 1.차원 축소
+#   고차원 데이터를 2D 또는 3D로 줄여 시각화 및 연산 효율성 개선
+# 2.노이즈 제거
+#   데이터의 주요 패턴을 유지하며 노이즈를 줄임
+# 3.특징 추출
+#   중요한 특성(주성분)을 추출하여 모델 성능 향상
+# n_iter=2500 → 2500번 반복하여 학습
+
 df_xy = pd.DataFrame({'words':labels, 'x':new_value[:, 0],
                       'y':new_value[:, 1]})
 df_xy.loc[df_xy.shape[0]] = (key_word, 0, 0)
