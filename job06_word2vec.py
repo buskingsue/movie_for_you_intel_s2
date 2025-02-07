52dd5312d34f253f519ac355af6242bae47cf1a9
buskingsue <abcdzip@naver.com>
2025-02-07 11:06:52 +0900

    word2vec 설명 추가

--- job06_word2vec.py
+++ job06_word2vec.py
@@ -15,6 +15,14 @@ print(tokens[0])
 
 embedding_model = Word2Vec(tokens, vector_size=100, window=4,
             min_count=20, workers=4, epochs=100, sg=1)
+# Word2Vec 모델을 학습
+# tokens: 학습할 문장 리스트
+# vector_size=100: 단어 임베딩 차원을 100으로 설정
+# window=4: 한 단어를 중심으로 좌우 4개의 단어까지 고려
+# min_count=20: 최소 20번 이상 등장한 단어만 학습에 사용
+# workers=4: CPU 코어 4개 사용
+# epochs=100: 100번 반복 학습
+# sg=1: Skip-gram 방식 사용 (0이면 CBOW)
 embedding_model.save('./models/word2vec_movie_review.model')
 print(list(embedding_model.wv.index_to_key))
 print(len(embedding_model.wv.index_to_key))
